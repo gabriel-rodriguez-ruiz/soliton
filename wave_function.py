b7f0c9819de5051ccc0fa1d77642d022bc90fc02
Gabriel F. Rodríguez Ruiz <gabrielrr96@gmail.com>
2023-01-31 22:07:28 -0300

    Now there are wavefunctions with arbitrary energy E.

--- wave_function.py
+++ wave_function.py
@@ -11,19 +11,29 @@ L = 10
 t_J = 0.1
 Delta = 1
 zeta = t_J/Delta
+E = 0.2  # si E+t_J/Delta>0
 
-def Psi(y):
+def Psi(y, i):
+    """Wavefunction with index i for a specific range of energy."""
+    chi_values = [(E+t_J)/Delta,  (E-t_J)/Delta, -(E+t_J)/Delta, -(E-t_J)/Delta]
+    chi = chi_values[i]
     if y<0:
-        return np.exp(zeta*y)
+        return np.exp(chi*y)
     elif y>=0 and y<=L:
-        return ((1-np.exp(zeta*L))/(1-np.exp(2*zeta*L))*(np.exp(zeta*y)
-                    +np.exp(-zeta*(y-L))))
+        #return ((1-np.exp(zeta*L))/(1-np.exp(2*zeta*L))*(np.exp(zeta*y)
+        #            +np.exp(-zeta*(y-L))))
+        #return 1/( np.exp(E/Delta*L)*np.sinh(t_J/Delta*L) )* \
+         #       ( np.exp(E/Delta*y)*np.sinh(t_J/Delta*y) - \
+          #       np.exp(E/Delta*(y+L))*np.sinh(t_J/Delta*(y-L)) )
+        return (1-np.exp(chi*L))/(1-np.exp(2*chi*L)) * (np.exp(chi*y)+np.exp(-chi*(y-L)))
     else:
-        return np.exp(-zeta*(y-L))
+        return np.exp(-chi*(y-L))
 
-y_values = np.linspace(-2*L,3*L, 100)
+y_values = np.linspace(L/2-2*L, L/2+2*L, 100)
 fig, ax = plt.subplots()
-ax.plot(y_values, [Psi(y) for y in y_values])
+for i in range(4):
+    ax.plot(y_values, [Psi(y, i) for y in y_values])
+ax.set_ylim([0,1])
 ax.set_ylabel(r"$\Psi$")
 ax.set_xlabel("y")
-ax.text(0, 0.5, f"L={L}, t_J={t_J}, Delta={Delta}")        
+ax.text(0, 0.5, rf"L={L}, $t_J$={t_J}, $\Delta$={Delta}, E={E}")        
