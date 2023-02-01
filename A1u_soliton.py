8a05a071fe0ceca95723920e81e07bde62d8d656
Gabriel <gabrielrr96@gmail.com>
2023-02-01 14:42:41 -0300

    First numerical calculation of soliton.

---
+++ A1u_soliton.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb  1 12:04:46 2023
+
+@author: gabriel
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+from hamiltonians import Hamiltonian_soliton_A1u
+from functions import probability_density, get_components
+
+L_x = 20
+L_y = 80
+t = 1
+Delta = 1
+mu = -1  #-2
+Phi = np.pi   #superconducting phase
+t_J = 1    #t/2
+index = 0   #which zero mode
+L = 60
+H = Hamiltonian_soliton_A1u(t=t, mu=mu, L_x=L_x, L_y=L_y, Delta=Delta, t_J=t_J, Phi=Phi, L=L)
+probability_density_2D, eigenvalues, eigenvectors = probability_density(H, L_x, L_y, index=index)
+
+#%%
+fig, ax = plt.subplots()
+image = ax.imshow(probability_density_2D, cmap="Blues", origin="lower") #I have made the transpose and changed the origin to have xy axes as usually
+plt.colorbar(image)
+#ax.set_title(f"{params}")
+ax.set_xlabel("x")
+ax.set_ylabel("y")
+ax.text(5,25, rf'$index={index}; \Phi={np.round(Phi, 2)}$')
+#plt.plot(probability_density[10,:,0])
+plt.tight_layout()
+
+#%% Energies
+ax2 = fig.add_axes([0.15, 0.15, 0.25, 0.25])
+ax2.scatter(np.arange(0, 4*L_x*L_y, 1), eigenvalues)
+ax2.set_xlim([2*(L_x*L_y-5), 2*(L_x*L_y+5)])
+ax2.set_ylim([-0.05, 0.05])
+
+#%% Spin determination
+from functions import mean_spin_xy, get_components
+
+zero_modes = eigenvectors[:, 2*(L_x*L_y-1):2*(L_x*L_y+1)]      #4 (2) modes with zero energy (with Zeeman)
+creation_up, creation_down, destruction_down, destruction_up = get_components(zero_modes[:,index], L_x, L_y)
+zero_plus_state = np.stack((destruction_up, destruction_down, creation_down, creation_up), axis=2) #positive energy eigenvector splitted in components
+# corner_state = zero_plus_state[L_x-2, L_y//2, :].reshape(4,1)  #positive energy point state localized at the junction
+# corner_state_normalized = corner_state/np.linalg.norm(corner_state[:2]) #normalization with only particle part
+zero_plus_state_normalized = zero_plus_state/np.linalg.norm(zero_plus_state[:,:,:2], axis=2, keepdims=True)
+
+# Spin mean value
+# spin_mean_value = mean_spin(corner_state_normalized)
+
+spin = mean_spin_xy(zero_plus_state_normalized)
+# fig, ax = plt.subplots()
+# image = ax.imshow(spin[:,:,2].T, cmap="Blues", origin="lower") #I have made the transpose and changed the origin to have xy axes as usually
+# plt.colorbar(image)
+#image.set_clim(np.min(spin[:,:,1].T), np.max(spin[:,:,1].T))
+
+# Meshgrid
+x, y = np.meshgrid(np.linspace(0, L_x-1, L_x), 
+                    #np.linspace(L_y-1, 0, L_y))
+                    np.linspace(0, L_y-1, L_y))
+
+
+  
+# Directional vectors
+u = spin[:, :, 0]   #x component
+v = spin[:, :, 1]   #y component
+
+# Plotting Vector Field with QUIVER
+ax.quiver(x, y, u, v, color='r', angles='uv', scale_units='xy', scale=1)
+ax.set_title('Spin Field in the plane')
+
+#%% Spin in z
+fig, ax = plt.subplots()
+image = ax.imshow(spin[:,:,2], cmap="Blues", origin="lower") #I have made the transpose and changed the origin to have xy axes as usually
+plt.colorbar(image)
+
+#%% Phi spectrum
+from functions import phi_spectrum
+
+Phi_values = np.linspace(0, 2*np.pi, 10)
+phi_energy = phi_spectrum(Hamiltonian_soliton_A1u, Phi_values, t, mu, L_x, L_y, Delta, t_J, L)
+
+fig, ax = plt.subplots()
+ax.plot(Phi_values, phi_energy)
+ax.set_xlabel(r"$\phi$")
+ax.set_ylabel("E")
+
---
+++ functions.py
@@ -0,0 +1,100 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb  1 12:05:43 2023
+
+@author: gabriel
+"""
+import numpy as np
+
+# Pauli matrices
+sigma_0 = np.eye(2)
+sigma_x = np.array([[0, 1], [1, 0]])
+sigma_y = np.array([[0, -1j], [1j, 0]])
+sigma_z = np.array([[1, 0], [0, -1]])
+tau_0 = np.eye(2)
+tau_x = np.array([[0, 1], [1, 0]])
+tau_y = np.array([[0, -1j], [1j, 0]])
+tau_z = np.array([[1, 0], [0, -1]])
+
+# Spin operators
+P = np.array([[1, 0, 0, 0], [0, 1, 0, 0]])  #particle projector
+# S_x = np.kron(tau_0, sigma_x)
+# S_y = np.kron(tau_0, sigma_y)
+# S_z = np.kron(tau_0, sigma_z)
+
+S_x = P.T@sigma_x@P
+S_y = P.T@sigma_y@P
+S_z = P.T@sigma_z@P
+
+# S_x = P.T@sigma_x@P
+# S_y = P.T@sigma_z@P
+# S_z = -P.T@sigma_y@P
+
+def spectrum(system, k_values, **params):
+    """Returns an array whose rows are the eigenvalues of the system for
+    a definite k. System should be a function that returns an array.
+    """
+    eigenvalues = []
+    for k in k_values:
+        params["k"] = k
+        H = system(**params)
+        energies = np.linalg.eigvalsh(H)
+        energies = list(energies)
+        eigenvalues.append(energies)
+    eigenvalues = np.array(eigenvalues)
+    return eigenvalues
+
+def mean_spin(state):
+    """Returns a 1D-array of length 3 with the spin mean value of the state.
+    State should be a vector of length 4.
+    """
+    spin_mean_value = np.concatenate([state.T.conj()@S_x@state,
+                                      state.T.conj()@S_y@state,
+                                      state.T.conj()@S_z@state])
+    return np.real(spin_mean_value/np.linalg.norm(spin_mean_value))
+
+def mean_spin_xy(state):
+    N_x, N_y, N_z = np.shape(state)     #N_z is always 4
+    spin_mean_value = np.zeros((N_x, N_y, 3))
+    for i in range(N_x):
+        for j in range(N_y):
+            for k in range(3):
+                spin_mean_value[i, j, k] = mean_spin(np.reshape(state[i,j,:], (4,1)))[k][0]
+    return spin_mean_value
+
+def get_components(state, L_x, L_y):
+    """
+    Get the components of components of the state: creation_up,
+    creation_down, destruction_down, destruction_up for a given
+    column state. Returns an array of shape (L_y, L_x)
+    """
+    destruction_up = state[0::4].reshape((L_x, L_y))
+    destruction_down = state[1::4].reshape((L_x, L_y))
+    creation_down = state[2::4].reshape((L_x, L_y))
+    creation_up = state[3::4].reshape((L_x, L_y))
+    return (np.flip(creation_up.T, axis=0),
+            np.flip(creation_down.T, axis=0),
+            np.flip(destruction_down.T, axis=0),
+            np.flip(destruction_up.T, axis=0))
+
+def probability_density(Hamiltonian, L_x, L_y, index):
+    """
+    Returns the probability density of a 2D system given a matrix Hamiltonian and the index of the zero mode (0<=index<=3).
+    The matrix element order are analogous to the real space grid.
+    """
+    eigenvalues, eigenvectors = np.linalg.eigh(Hamiltonian)
+    #zero_modes = eigenvectors[:, 2*(L_x*L_y-1):2*(L_x*L_y+1)]      #4 (2) modes with zero energy (with Zeeman)
+    a, b, c, d = get_components(eigenvectors[:,2*(L_x*L_y-1)+index], L_x, L_y)
+    probability_density = np.abs(a)**2 + np.abs(b)**2 + np.abs(c)**2 + np.abs(d)**2
+    return probability_density, eigenvalues, eigenvectors
+
+def phi_spectrum(Hamiltonian, Phi, t, mu, L_x, L_y, Delta, t_J, L):
+    """
+    Returns the phi spectrum for the six lowest energies.
+    """
+    energies = []
+    for Phi_value in Phi:
+        eigenvalues = np.linalg.eigvalsh(Hamiltonian(t=t, mu=mu, L_x=L_x, L_y=L_y, Delta=Delta, t_J=t_J, Phi=Phi_value, L=L))
+        energies.append(eigenvalues[2*(L_x*L_y-3):2*(L_x*L_y+3)])
+    return energies
