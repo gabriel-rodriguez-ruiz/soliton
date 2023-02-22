88d74b8d6525571de24d7891e75017e884a2fce1
Gabriel <gabrielrr96@gmail.com>
2023-02-22 15:20:07 -0300

    All k's calculation.

--- A1u_S_junction.py
+++ A1u_S_junction.py
@@ -8,12 +8,12 @@ Created on Fri Feb 10 12:14:56 2023
 
 import numpy as np
 import matplotlib.pyplot as plt
-from hamiltonians import Hamiltonian_A1u_S, Zeeman
+from hamiltonians import Hamiltonian_A1u_S, Zeeman, Hamiltonian_A1u_S_double_soliton
 from functions import get_components
 import scipy
 
-L_x = 50
-L_y = 400
+L_x = 100
+L_y = 100
 t = 1
 Delta = 1
 mu = -2
@@ -23,22 +23,34 @@ k = 4
 theta = np.pi/2
 phi = np.pi/2
 Delta_Z = 0
+L = L_y//2
 
-H = Hamiltonian_A1u_S(t=t, mu=mu, L_x=L_x, L_y=L_y, Delta=Delta, t_J=t_J, Phi=Phi) + Zeeman(theta=theta, phi=phi, Delta_Z=Delta_Z, L_x=L_x, L_y=L_y)
+#H = Hamiltonian_A1u_S(t=t, mu=mu, L_x=L_x, L_y=L_y, Delta=Delta, t_J=t_J, Phi=Phi) + Zeeman(theta=theta, phi=phi, Delta_Z=Delta_Z, L_x=L_x, L_y=L_y)
+H = Hamiltonian_A1u_S_double_soliton(t, mu, L_x, L_y, Delta, t_J, Phi, L)
 eigenvalues_sparse, eigenvectors_sparse = scipy.sparse.linalg.eigsh(H, k=k, sigma=0) 
 
 #%% Probability density
-index = 1   #which zero mode (less than k)
-a, b, c, d = get_components(eigenvectors_sparse[:,index], L_x, L_y)
-probability_density_2D = np.abs(a)**2 + np.abs(b)**2 + np.abs(c)**2 + np.abs(d)**2
+index = np.arange(k)   #which zero mode (less than k)
+probability_density = []
+zero_state = []
+localized_state_up = [] # it is the site ((L_x+L)/2, L_y/2)
+localized_state_down = []
+for i in index:
+    destruction_up, destruction_down, creation_down, creation_up = get_components(eigenvectors_sparse[:,i], L_x, L_y)
+    probability_density.append(np.abs(destruction_up)**2 + np.abs(destruction_down)**2 + np.abs(creation_down)**2 + np.abs(creation_up)**2)
+    zero_state.append(np.stack((destruction_up, destruction_down, creation_down, creation_up), axis=2)) #positive energy eigenvector splitted in components
+    localized_state_up.append(zero_state[i][(L_y-L)//2, L_x-2,:])
+    localized_state_down.append(zero_state[i][(L_y+L)//2, L_x-2,:])
 
-fig, ax = plt.subplots(num="Probability density", clear=True)
-image = ax.imshow(probability_density_2D, cmap="Blues", origin="lower") #I have made the transpose and changed the origin to have xy axes as usually
+#%%
+index = 0
+fig, ax = plt.subplots(num="Probability density", clear=False)
+image = ax.imshow(probability_density[index], cmap="Blues", origin="lower") #I have made the transpose and changed the origin to have xy axes as usually
 plt.colorbar(image)
 #ax.set_title(f"{params}")
 ax.set_xlabel("x")
 ax.set_ylabel("y")
-ax.text(5,25, rf'$t_J={t_J}; \Phi={np.round(Phi, 2)}$')
+ax.text(5,25, rf'$t_J={t_J}; \Phi={np.round(Phi, 2)}$; index={index}')
 #plt.plot(probability_density[10,:,0])
 plt.tight_layout()
 #%% Energies
@@ -53,10 +65,6 @@ from functions import mean_spin, mean_spin_xy, get_components
 zero_modes = eigenvectors_sparse      #4 (2) modes with zero energy (with Zeeman)
 creation_up, creation_down, destruction_down, destruction_up = get_components(zero_modes[:,index], L_x, L_y)
 zero_state = np.stack((destruction_up, destruction_down, creation_down, creation_up), axis=2) #positive energy eigenvector splitted in components
-corner_state = zero_state[L_y//2, L_x-2, :].reshape(4,1)  #positive energy point state localized at the junction
-
-# Spin mean value
-spin_mean_value = mean_spin(corner_state)
 
 spin = mean_spin_xy(zero_state)
 # fig, ax = plt.subplots()
