77c47b165301fc0867fad40592b69a402e6fc3d8
Gabriel F. Rodríguez Ruiz <gabrielrr96@gmail.com>
2023-04-16 13:02:48 -0300

    Mass soliton drawings.

--- drawings.py
+++ drawings.py
@@ -29,25 +29,24 @@ def Phi(epsilon, y):
     """
     return np.pi + np.sign(y)*epsilon
 
-def mass(phi, y):
+def mass(phi, t_J):
     """
     Mass term.    
 
     Parameters
     ----------
     phi : float
-        Phase difference.
-    y : float
-        Coordinate along the junction.
-
+        Phase difference for a given y.
+    t_J : float
+        Hopping parameter.
     Returns
     -------
-    None.
+    The mass term.
 
     """
+    return t_J*np.cos(phi/2)
 
 #%% Plotting of flux
-
 epsilon = 0.04*np.pi
 y = np.linspace(-1, 1)
 
@@ -57,5 +56,17 @@ ax.set_xlabel("y")
 ax.set_ylabel(r"$\phi(y)$")
 ax.set_yticks([np.pi-epsilon, np.pi, np.pi+epsilon],
               [r"$\pi-\epsilon$", r"$\pi$", r"$\pi+\epsilon$"])
+ax.set_xticks([0],["0"])
+fig.tight_layout()
+
+#%% Plotting of the mass
+t_J = 1
+
+fig, ax = plt.subplots()
+ax.plot(y, [mass(Phi(epsilon, y_value), t_J) for y_value in y])
+ax.set_xlabel("y")
+ax.set_ylabel(r"$m(y)$")
+ax.set_yticks([mass(np.pi-epsilon, t_J), 0, mass(np.pi+epsilon, t_J)],
+              [r"$t_J\sin(\epsilon/2)$", r"0", r"$-t_J\sin(\epsilon/2)$"])
 ax.set_xticks([0],["0"])
 fig.tight_layout()
