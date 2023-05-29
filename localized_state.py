Major changes. I have taken into account the conjugate when I do an internal product. Threfor the eta in the paper is wrong (one should change right with left). I have check the numerical and analytical phase differences by letting the aanalytical solution be the same and changing left with right in the numerics.

localized_state.py
@@ -147,8 +147,10 @@ H = Hamiltonian_soliton_A1u_sparse(t=t, mu=mu, L_x=L_x, L_y=L_y, Delta=Delta, t_
 params = {"t": t, "mu": mu, "L_x": L_x, "L_y": L_y, "Delta": Delta, "t_J": t_J, "Phi": Phi, "L": L}
 eigenvalues_sparse, eigenvectors_sparse = scipy.sparse.linalg.eigsh(H, k=k, sigma=0) 
 idx = eigenvalues_sparse.argsort()
-eigenvalues_sparse = eigenvalues_sparse[idx]
-eigenvectors_sparse = eigenvectors_sparse[:, idx]
+eigenvalues_sparse_sorted = eigenvalues_sparse[idx]
+eigenvectors_sparse_sorted = eigenvectors_sparse[:, idx]
+eigenvalues_sparse = eigenvalues_sparse_sorted
+eigenvectors_sparse = eigenvectors_sparse_sorted
 
 index = np.arange(k)   #which zero mode (less than k)
 probability_density = []
@@ -204,17 +206,91 @@ for i in range(k):
     spin.append(mean_spin_xy(zero_state[i]))
 
 #%% down minus left
-# Las definiciones de cada componente cambian en cada corrida
-up_plus_left_particle = np.array(localized_state_left[2][:,0])
-up_plus_right_particle = np.array(localized_state_right[2][:,0])
-
+up_plus_left_particle = np.array(localized_state_right[2][:,0])
+# Hice un cambio de left por right en la solución numérica
 fig, ax = plt.subplots()
 y = np.arange(-L_y//2+L//2, L_y//2+L//2)
 ax.plot(y, np.real(up_plus_left_particle), "--", label="Real numerical")
 ax.plot(y, np.imag(up_plus_left_particle), "--", label="Imaginary numerical")
 
-phase = 1j*np.exp(1j*(-0.1+np.angle(up_plus_left_particle[(L_y-L)//2]) + np.pi/2))
-up_plus_right_particle_analytical = [phase/np.sqrt(2)*psi_3_plus(y_value, kappa, m_0, Delta, L) for y_value in y]
+phase_up_plus_right_particle = 1j*np.exp(1j*(np.angle(up_plus_left_particle[(L_y-L)//2]) + np.pi/2))
+up_plus_right_particle_analytical = [phase_up_plus_right_particle/np.sqrt(2)*psi_3_plus(y_value, kappa, m_0, Delta, L) for y_value in y]
 ax.plot(y, np.real(up_plus_right_particle_analytical), label="Real analytical")
 ax.plot(y, np.imag(up_plus_right_particle_analytical), label="Imaginary analytical")
 ax.legend()
+plt.title("up_plus_right_particle_analytical")
+
+#%%
+up_plus_right_particle = np.array(localized_state_left[2][:,0])
+fig, ax = plt.subplots()
+y = np.arange(-L_y//2+L//2, L_y//2+L//2)
+ax.plot(y, np.real(up_plus_right_particle), "--", label="Real numerical")
+ax.plot(y, np.imag(up_plus_right_particle), "--", label="Imaginary numerical")
+
+phase_up_plus_left_particle = np.exp(1j*(np.angle(up_plus_right_particle[(L_y-L)//2]) + np.pi/2))
+up_plus_left_particle_analytical = [phase_up_plus_left_particle/np.sqrt(2)*psi_1_plus(y_value, kappa, m_0, Delta, L) for y_value in y]
+ax.plot(y, np.real(up_plus_left_particle_analytical), label="Real analytical")
+ax.plot(y, np.imag(up_plus_left_particle_analytical), label="Imaginary analytical")
+ax.legend()
+ax.set_xlabel("y")
+plt.title("up_plus_left_particle_analytical")
+
+#%%
+up_plus_left_hole = np.array(localized_state_right[2][:,3])
+fig, ax = plt.subplots()
+y = np.arange(-L_y//2+L//2, L_y//2+L//2)
+ax.plot(y, np.real(up_plus_left_hole), "--", label="Real numerical")
+ax.plot(y, np.imag(up_plus_left_hole), "--", label="Imaginary numerical")
+
+phase = -1j*phase_up_plus_right_particle
+up_plus_right_hole_analytical = [phase/np.sqrt(2)*psi_3_plus(y_value, kappa, m_0, Delta, L) for y_value in y]
+ax.plot(y, np.real(up_plus_right_hole_analytical), label="Real analytical")
+ax.plot(y, np.imag(up_plus_right_hole_analytical), label="Imaginary analytical")
+ax.legend()
+ax.set_xlabel("y")
+plt.title("up_plus_right_hole_analytical")
+
+#%%
+up_plus_right_hole = np.array(localized_state_left[2][:,3])
+fig, ax = plt.subplots()
+y = np.arange(-L_y//2+L//2, L_y//2+L//2)
+ax.plot(y, np.real(up_plus_right_hole), "--", label="Real numerical")
+ax.plot(y, np.imag(up_plus_right_hole), "--", label="Imaginary numerical")
+
+phase = 1j*phase_up_plus_left_particle
+up_plus_left_hole_analytical = [phase/np.sqrt(2)*psi_1_plus(y_value, kappa, m_0, Delta, L) for y_value in y]
+ax.plot(y, np.real(up_plus_left_hole_analytical), label="Real analytical")
+ax.plot(y, np.imag(up_plus_left_hole_analytical), label="Imaginary analytical")
+ax.legend()
+ax.set_xlabel("y")
+plt.title("up_plus_left_hole_analytical")
+
+#%%
+down_plus_left_particle = np.array(localized_state_right[3][:,1])
+# Hice un cambio de left por right en la solución numérica
+fig, ax = plt.subplots()
+y = np.arange(-L_y//2+L//2, L_y//2+L//2)
+ax.plot(y, np.real(down_plus_left_particle), "--", label="Real numerical")
+ax.plot(y, np.imag(down_plus_left_particle), "--", label="Imaginary numerical")
+
+phase_down_plus_right_particle = -np.exp(1j*(np.angle(down_plus_left_particle[(L_y-L)//2]) + np.pi/2))
+down_plus_right_particle_analytical = [phase_down_plus_right_particle/np.sqrt(2)*psi_4_plus(y_value, kappa, m_0, Delta, L) for y_value in y]
+ax.plot(y, np.real(down_plus_right_particle_analytical), label="Real analytical")
+ax.plot(y, np.imag(down_plus_right_particle_analytical), label="Imaginary analytical")
+ax.legend()
+plt.title("down_plus_right_particle_analytical")
+
+#%%
+down_plus_left_hole = np.array(localized_state_right[3][:,2])
+# Hice un cambio de left por right en la solución numérica
+fig, ax = plt.subplots()
+y = np.arange(-L_y//2+L//2, L_y//2+L//2)
+ax.plot(y, np.real(down_plus_left_hole), "--", label="Real numerical")
+ax.plot(y, np.imag(down_plus_left_hole), "--", label="Imaginary numerical")
+
+phase_down_plus_right_hole = -1j*phase_down_plus_right_particle
+down_plus_right_hole_analytical = [phase_down_plus_right_hole/np.sqrt(2)*psi_4_plus(y_value, kappa, m_0, Delta, L) for y_value in y]
+ax.plot(y, np.real(down_plus_right_hole_analytical), label="Real analytical")
+ax.plot(y, np.imag(down_plus_right_hole_analytical), label="Imaginary analytical")
+ax.legend()
+plt.title("down_plus_right_hole_analytical")

symmetry.py
@@ -31,7 +31,7 @@ eigenvectors_sparse = eigenvectors_sparse[:, idx]
 
 from scipy.linalg import orth
 eigenvectors_negative = scipy.linalg.orth(eigenvectors_sparse[:,:2]) #for negative eigenvalues
-
+#eigenvectors_negative = eigenvectors_sparse[:,:2]
 C = charge_conjugation_operator_sparse(L_x, L_y)
 A = C.dot(eigenvectors_negative[:,0].conj()) #positive eigenvector
 is_charge_conjugation_A = np.allclose(H @ A, -eigenvalues_sparse[0]*A)
@@ -40,3 +40,30 @@ is_charge_conjugation_B = np.allclose(H @ B, -eigenvalues_sparse[1]*B)
 eigenvectors = np.append(eigenvectors_negative, A.reshape((len(A), 1)), axis=1)
 eigenvectors = np.append(eigenvectors, B.reshape((len(B), 1)), axis=1)
 
+eigenvector_up_negative = 1/np.sqrt(2)*(eigenvectors[:,0]+eigenvectors[:,1])
+eigenvector_down_negative = 1/np.sqrt(2)*(eigenvectors[:,0]-eigenvectors[:,1])
+eigenvector_up_positive = 1/np.sqrt(2)*(eigenvectors[:,2]+eigenvectors[:,3])
+eigenvector_down_positive = 1/np.sqrt(2)*(eigenvectors[:,2]-eigenvectors[:,3])
+
+eigenvectors_polarized = np.array([eigenvector_up_negative, eigenvector_down_negative, eigenvector_up_positive, eigenvector_down_positive]).T
+
+index = np.arange(k)   #which zero mode (less than k)
+probability_density = []
+zero_state = []
+localized_state_upper_left = [] # it is the site (L_x/2-1, (L_y+L)/2)
+localized_state_upper_right = [] # it is the site (L_x/2, (L_y+L)/2)
+localized_state_bottom_left = [] # it is the site (L_x/2-1, (L_y-L)/2)
+localized_state_bottom_right = [] # it is the site (L_x/2, (L_y-L)/2)
+localized_state_left = []
+localized_state_right = []
+
+for i in index:
+    destruction_up, destruction_down, creation_down, creation_up = get_components(eigenvectors_sparse[:,i], L_x, L_y)
+    probability_density.append((np.abs(destruction_up)**2 + np.abs(destruction_down)**2 + np.abs(creation_down)**2 + np.abs(creation_up)**2)/(np.linalg.norm(np.abs(destruction_up)**2 + np.abs(destruction_down)**2 + np.abs(creation_down)**2 + np.abs(creation_up)**2)))
+    zero_state.append(np.stack((destruction_up, destruction_down, creation_down, creation_up), axis=2)) #positive energy eigenvector splitted in components
+    localized_state_upper_left.append(zero_state[i][(L_y+L)//2, L_x//2-1,:])
+    localized_state_upper_right.append(zero_state[i][(L_y+L)//2, L_x//2,:])
+    localized_state_bottom_left.append(zero_state[i][(L_y-L)//2, L_x//2-1,:])
+    localized_state_bottom_right.append(zero_state[i][(L_y-L)//2, L_x//2,:])
+    localized_state_left.append(zero_state[i][:, L_x//2-1,:])
+    localized_state_right.append(zero_state[i][:, L_x//2,:])